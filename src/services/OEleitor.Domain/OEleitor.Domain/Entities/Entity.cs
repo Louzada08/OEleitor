b0e90c7712a55dc1a362628612aa63dd1950b4d2
anderson louzada <anderson_louzada@hotmail.com>
2022-12-17 20:06:32 -0300

    Criando a classe Entity base

--- src/services/OEleitor.Domain/OEleitor.Domain/Entities/Entity.cs
+++ src/services/OEleitor.Domain/OEleitor.Domain/Entities/Entity.cs
@@ -1,7 +1,44 @@
-﻿namespace OEleitor.Domain.Entities
+﻿using FluentValidation.Results;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OEleitor.Domain.Entities
 {
     public abstract class Entity
     {
         public long Id { get; private set; }
+
+        [NotMapped]
+        public ValidationResult ValidationResult { get; private set; }
+
+        private List<INotification> _domainEvents;
+        public IReadOnlyCollection<INotification> DomainEvents => _domainEvents?.AsReadOnly();
+        public DateTime DataCadastro { get; protected set; }
+        public DateTime DataAlteracao { get; protected set; }
+        public DateTime DataExclusao { get; protected set; }
+
+        protected Entity()
+        {
+            ValidationResult = new ValidationResult();
+
+            DataCadastro = BrazilianDateTime();
+            DataAlteracao = BrazilianDateTime();
+            DataExclusao = BrazilianDateTime();
+        }
+
+        public DateTime BrazilianDateTime()
+        {
+            DateTime dateTime = DateTime.UtcNow;
+            TimeZoneInfo horaBrasilia = TimeZoneInfo.FindSystemTimeZoneById("E. South America Standard Time");
+            return TimeZoneInfo.ConvertTimeFromUtc(dateTime, horaBrasilia);
+        }
+
+        public void AddDomainEvent(INotification eventItem)
+        {
+            _domainEvents = _domainEvents ?? new List<INotification>();
+            _domainEvents.Add(eventItem);
+        }
     }
 }
